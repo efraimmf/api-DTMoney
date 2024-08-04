6b301211fb7cdc3b433d355124be3fdc5f9c9274
efraimff <emfs6@aluno.ifal.edu.br>
2024-08-03 22:50:56 -0300

    feat: add search by title, update, and delete transaction functionality
    - Added method to search transactions by title
    - Added method to update a transaction
    - Added method to delete a transaction
    - Created updateTransaction DTO

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { AppService } from './app.service';
 import { CreateTransactionDto } from "./dto/createTransaction.dto";
+import { UpdateTransactionDto } from "./dto/updateTransaction.dto";
 
 @Controller('/app')
 export class AppController {
@@ -9,6 +10,30 @@ export class AppController {
   @Post('/transaction')
   async createTransaction(@Body() data: CreateTransactionDto) {
     const transaction = await this.appService.createTransaction(data);
+    return transaction;
+  }
+
+  @Get('/transactions')
+  async getAllTransactions(){
+    const transactions = await this.appService.getAllTransactions();
+    return transactions;
+  }
+
+  @Get('/transactions/:title')
+  async getTransactionByTitle(@Param('title') title: string) {
+    const transaction = await this.appService.getTransactionByTitle(title);
+    return transaction;
+  }
+
+  @Put('/transactions/:id')
+  async updateTransaction(@Param('id') id: string, @Body() data: UpdateTransactionDto) {
+    const transaction = await this.appService.updateTransaction(id, data);
+    return transaction;
+  }
+
+  @Delete('/transactions/:id')
+  async deleteTransaction(@Param('id') id: string) {
+    const transaction = await this.appService.deleteTransaction(id);
     return transaction;
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from "./prisma.service";
 import { CreateTransactionDto } from "./dto/createTransaction.dto";
+import { UpdateTransactionDto } from "./dto/updateTransaction.dto";
 
 @Injectable()
 export class AppService {
   constructor(private readonly prismaService: PrismaService) {}
 
     async createTransaction(data: CreateTransactionDto){
-      const createdTransaction = await this.prismaService.transaction.create({data})
-      return createdTransaction;
+      const transaction = await this.prismaService.transaction.create({data})
+      return transaction;
+    }
+
+    async getAllTransactions(){
+      const transactions = await this.prismaService.transaction.findMany();
+      return transactions;
+    }
+
+    async getTransactionByTitle (title: string){
+      const transaction = await this.prismaService.transaction.findMany({
+        where:{
+          title: {contains: title, mode: 'insensitive'}
+        }
+      });
+
+      return transaction;
+    }
+
+    async updateTransaction(id: string, data: UpdateTransactionDto){
+      const transaction = await this.prismaService.transaction.update({
+        where: {id: id},
+        data: data});
+
+      return transaction;
+    }
+
+    async deleteTransaction(id: string){
+      const transaction = await this.prismaService.transaction.delete({
+        where:{id: id}});
+
+      return transaction;
     }
 }
